dc78ff78d34fb8a2f696c2ed6ebc81a26c459a27
Praktikant T <Tobias.rothmann@outlook.com>
2018-01-31 11:31:27 +0100

    Initial Commit

---
+++ FridgeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FridgeApp
+//
+//  Created by Interne Apps on 31.01.18.
+//  Copyright © 2018 PraktikantT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
