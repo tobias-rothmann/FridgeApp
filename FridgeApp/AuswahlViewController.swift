188b7def5554bd9c0742a0698ac80c759719cb6d
Praktikant T <Tobias.rothmann@outlook.com>
2018-01-31 13:27:09 +0100

    Working, AuswahlViewControlleris ready, TableView is in progress

---
+++ FridgeApp/AuswahlViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AuswahlViewController.swift
+//  FridgeApp
+//
+//  Created by Interne Apps on 31.01.18.
+//  Copyright © 2018 PraktikantT. All rights reserved.
+//
+
+import UIKit
+
+class AuswahlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
